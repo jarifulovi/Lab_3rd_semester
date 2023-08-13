9184cf03a84d95def01db0df03534a14ce5a4d97
ovi <jarifulovi@gmail.com>
2023-08-13 13:01:09 +0600

    updated 8/13/2023

--- Theory of Computing/NFA.c
+++ Theory of Computing/NFA.c
@@ -4,7 +4,14 @@
 
 #define MAX_STATES 3
 #define MAX_ALPHABET 2
+#define STRING 6
 int numpaths = 0;
+char allpaths[MAX_STATES*STRING][100];
+char path[STRING+1] = "";
+
+void print_path(char path[100]){
+    printf("%s\n",path);
+}
 
 int stateIndex(char state, char *states) {
     for(int i=0;states[i]!='\0';i++){
@@ -12,25 +19,27 @@ int stateIndex(char state, char *states) {
     }
 }
 
-bool NFA(char currentState, char *input, char *alphabet, char acceptState,
-                 char transition[MAX_STATES][MAX_ALPHABET][MAX_STATES+1],char *states,char* path,char (*allpaths)[MAX_STATES],int pathlength) {
-    if (*input == '\0') {
-        strcpy(allpaths[numpaths++],path);
+bool NFA(char currentState, char *string, char *alphabet, char acceptState,
+                 char transition[MAX_STATES][MAX_ALPHABET][MAX_STATES+1],char *states,int pathlength) {
+    path[pathlength] = currentState;
+    if (*string == '\0') {
+        path[pathlength+1] = '\0';
+        print_path(path);
         return currentState == acceptState;
     }
 
     bool result = false;
     for (int i = 0; i < MAX_STATES; i++) {
-        char nextState = transition[stateIndex(currentState, states)][*input-'0'][i];
+        char nextState = transition[stateIndex(currentState, states)][*string-'0'][i];
         if (nextState != '\0') {
-            path[pathlength] = currentState;
-            path[pathlength+1] = '\0';
-            result = result || NFA(nextState, input + 1,
-                                          alphabet, acceptState, transition,states,path,allpaths,pathlength+1);
-            path[pathlength] = '\0';
+            
+            result = result || NFA(nextState, string + 1,
+                                          alphabet, acceptState, transition,states,pathlength+1);
+           
         }
+        
     }
-
+    
     return result;
 }
 
@@ -60,18 +69,16 @@ int main() {
             scanf("%s", transition[i][j]);
         }
     }
-
-    char string[] = "1111011";
+    char string[100];
     printf("Enter the input string: ");
     scanf("%s", string);
 
-    char allpaths[MAX_STATES][strlen(string)+1];
-    char path[strlen(string)+1];
+    
 
-    if (NFA(startState, string, alphabet, acceptState, transition,states,path,allpaths,0)) {
-        printf("Input string is accepted by the NFA.\n");
+    if (NFA(startState, string, alphabet, acceptState, transition,states,0)) {
+        printf("\nInput string is accepted by the NFA.\n");
     } else {
-        printf("Input string is rejected by the NFA.\n");
+        printf("\nInput string is rejected by the NFA.\n");
     }
     
     return 0;
